a4b2caed87cac55648e7057b463b533d9cfeb55f
sir-shalahuddin <sirajshalahuddin@gmail.com>
2024-09-14 11:12:34 +0700

    add logger

--- userservice/cmd/rest.go
+++ userservice/cmd/rest.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	router "github.com/sir-shalahuddin/grpc-learn/userservice/internal/routes"
 )
 
@@ -13,6 +14,8 @@ func StartRESTServer(db *sql.DB, port string, jwtSecret string) {
 	app := fiber.New()
 
 	app.Use(cors.New())
+
+	app.Use(logger.New())
 
 	router.RegisterRoutes(app, db, jwtSecret)
 	err := (app.Listen(":" + port))
